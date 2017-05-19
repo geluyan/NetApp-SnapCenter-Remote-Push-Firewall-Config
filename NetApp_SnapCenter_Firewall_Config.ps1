e99e7d2c93e1021a68f478b963bdc01b9ac1dd04
geluyan <a.geluykens@gmail.com>
2017-05-19 14:13:33 +0200

    Add files via upload

---
+++ NetApp_SnapCenter_Firewall_Config.ps1
@@ -0,0 +1,21 @@
+#####################################################
+# NetApp SnapCenter Firewall Config
+# Created by: Andy Geluykens
+# Version: 1.0
+# Description:
+#
+# This script will insert all required Firewall rules 
+# for NetApp SnapCenter remote push to work
+#####################################################
+
+#SnapCenter IP Address
+$snapcenterServer = 10.4.192.59
+
+# DO NOT EDIT BELOW #
+#####################
+
+New-NetFirewallRule -DisplayName "SnapCenter (SMCore HTTP Communication Port)" -Action Allow -Direction Inbound -Enabled True -Group "NetApp SnapCenter" -LocalPort 8145 -Profile Any -Protocol TCP -RemoteAddress $snapcenterServer
+New-NetFirewallRule -DisplayName "SnapCenter (NetBios)" -Action Allow -Direction Inbound -Enabled True -Group "NetApp SnapCenter" -LocalPort 137 -Profile Any -Protocol UDP -RemoteAddress $snapcenterServer
+New-NetFirewallRule -DisplayName "SnapCenter (DCE/RPC)" -Action Allow -Direction Inbound -Enabled True -Group "NetApp SnapCenter" -LocalPort 135 -Profile Any -Protocol TCP -RemoteAddress $snapcenterServer
+New-NetFirewallRule -DisplayName "SnapCenter (SMB-In)" -Action Allow -Direction Inbound -Enabled True -Group "NetApp SnapCenter" -LocalPort 445 -Profile Any -Protocol TCP -RemoteAddress $snapcenterServer
+New-NetFirewallRule -DisplayName "SnapCenter (Background Intelligent Transfer Service [BITS ])" -Action Allow -Direction Inbound -Enabled True -Group "NetApp SnapCenter" -Profile Any -RemoteAddress $snapcenterServer -Service BITS
